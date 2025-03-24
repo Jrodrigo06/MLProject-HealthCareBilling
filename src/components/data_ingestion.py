561dc6c1906fa306ce1fca1c3d8ced225a21d25c
Jerome Rodrigo <jeromerodrigo06@gmail.com>
2025-03-24 16:50:15 -0400

    Finished data ingestion

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -24,6 +24,28 @@ class DataIngestion:
             df=pd.read_csv('notebook\data\insurance.csv') 
             logging.info('Read the dataset as dataframe')
 
-            os.makedirs(self.ingestion_config.train_data_path)
-        except:
-            pass
+            os.makedirs(os.path.dirname(self.ingestion_config.train_data_path), exist_ok=True)
+            
+            df.to_csv(self.ingestion_config.raw_data_path, index = False, header = True)
+
+            logging.info("Train Test Split iniated")
+            train_set,test_set=train_test_split(df,test_size=0.2,random_state=42)
+
+            train_set.to_csv(self.ingestion_config.train_data_path, index = False, header = True)
+            
+            test_set.to_csv(self.ingestion_config.test_data_path, index = False, header = True)
+
+            logging.info("Ingestion of the data is completed")
+
+            return(
+                self.ingestion_config.train_data_path,
+                self.ingestion_config.test_data_path
+
+
+            )
+        except Exception as e:
+            raise customException(e,sys)
+        
+if __name__== "__main__":
+    obj=DataIngestion()
+    obj.initiate_data_ingestion()
