a6eaab009e5971c9effef4ddf8e1d2b3eae068be
Jerome Rodrigo <jeromerodrigo06@gmail.com>
2025-04-27 02:20:25 -0400

    Added error catching for internal server error

--- app.py
+++ app.py
@@ -18,23 +18,30 @@ def index():
 
 @app.route('/predictdata', methods=['GET','POST'])
 def predict_datapoint():
-    if request.method=='GET':
-        return render_template('home.html', results = None)
-    else:
-        data=CustomData(
-            age = request.form.get('age'),
-            sex = request.form.get('sex'),
-            bmi = float(request.form.get('bmi')),
-            smoker_binary = int(request.form.get('smoker_binary')),
-            region = request.form.get('region')
-        )
+    try:
+        if request.method=='GET':
+            return render_template('home.html', results = None)
+        else:
+            data=CustomData(
+                age = request.form.get('age'),
+                sex = request.form.get('sex'),
+                bmi = float(request.form.get('bmi')),
+                smoker_binary = int(request.form.get('smoker_binary')),
+                region = request.form.get('region')
+            )
 
-        pred_df = data.get_data_as_data_frame()
-        print(pred_df)
+            pred_df = data.get_data_as_data_frame()
+            print(pred_df)
 
-        predict_pipeline = PredictPipeline()
-        results = predict_pipeline.predict(pred_df)
-        return render_template('home.html', results = results[0])
+            predict_pipeline = PredictPipeline()
+            results = predict_pipeline.predict(pred_df)
+            return render_template('home.html', results = results[0])
+    except Exception as e:
+        import traceback
+        print("==== ERROR OCCURRED DURING PREDICTION ====")
+        print(e)
+        traceback.print_exc()
+        return f"Internal server error: {e}", 500
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8000)
